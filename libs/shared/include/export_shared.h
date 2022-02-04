c96024629d1805aa9f48411ffc3b26e52ff6ce65
unknown <cherif@beijin.unice.fr>
2022-02-05 00:18:21 +0100

    adding lx-geo-shared lib static parameter

--- libs/shared/include/export_shared.h
+++ libs/shared/include/export_shared.h
@@ -1,7 +1,12 @@
 #pragma once
+#ifdef LXGEO_FACTORY_STATIC
+#define LX_GEO_FACTORY_SHARED_API  
+#else
 
-#ifdef LxGeoFactoryShared_EXPORTS
+#ifdef LXGEO_FACTORY_SHARED_EXPORTS
 #define LX_GEO_FACTORY_SHARED_API __declspec(dllexport)
 #else
 #define LX_GEO_FACTORY_SHARED_API __declspec(dllimport)
+#endif
+
 #endif
