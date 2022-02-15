e2666e5254b8c10ed8f7c8858ab9a9b789500d51
unknown <cherif@beijin.unice.fr>
2022-02-15 18:25:53 +0100

    overloading operators of SpatialCoords struct

--- libs/shared/include/coords.h
+++ libs/shared/include/coords.h
@@ -3,6 +3,10 @@
 struct SpatialCoords {
 	double xc;
 	double yc;
+
+	SpatialCoords& operator+=(const SpatialCoords& rhs) { xc += rhs.xc; yc += rhs.yc; return *this; }
+	SpatialCoords& operator/=(const double& N) { xc /= N; yc /= N; return *this; }
+	SpatialCoords operator/(const double& N) { return { xc / N, yc / N }; }
 };
 
 struct PixelCoords {
