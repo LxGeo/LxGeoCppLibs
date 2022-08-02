56334bd7cc1a9fd85b1e8ceec2a5d6ca382456b3
unknown <cherif@beijin.unice.fr>
2022-08-02 10:56:17 +0200

    adding geometry translation operation to affine transformer using bg:matrix_transformer and spatial coords

--- libs/shared/include/affine_geometry/affine_transformer.h
+++ libs/shared/include/affine_geometry/affine_transformer.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "defs.h"
+#include <boost/numeric/ublas/operation.hpp>
+
 
 namespace LxGeo
 {
@@ -17,21 +19,70 @@ namespace LxGeo
 		template<typename geometry_type>
 		std::vector<geometry_type> translate_geometries(std::vector<geometry_type>& input_container, std::vector<SpatialCoords>& displacement_container) {
 
-			assert(input_container.size() == displacement_container.size(), "Size mismatch of input containers!");
+			assert(input_container.size() == displacement_container.size() || displacement_container.size()==1, "Size mismatch of input containers!");
 			std::vector<geometry_type> out_container; out_container.reserve(input_container.size());
 
-			std::vector<geometry_type>::iterator it1 = input_container.begin();
-			std::vector<SpatialCoords>::iterator it2 = displacement_container.begin();
+			auto it1 = input_container.begin();
+			auto it2 = displacement_container.begin();
+
+			while (it1 != input_container.end() && it2 != displacement_container.end()) {
+				bg::strategy::transform::translate_transformer<double, 2, 2> trans_obj(it2->xc, it2->yc);
+				geometry_type transformed_geom = translate_geometry(*it1, trans_obj);
+				out_container.push_back(transformed_geom);
+				it1++;
+				if (displacement_container.size() != 1) it2++;
+			}
+			return out_container;
+
+		}
+
+
+		template<typename geometry_type>
+		std::list<geometry_type> translate_geometries(std::list<geometry_type>& input_container, std::list<SpatialCoords>& displacement_container) {
+
+			assert(input_container.size() == displacement_container.size() || displacement_container.size() == 1, "Size mismatch of input containers!");
+			std::list<geometry_type> out_container;
+			
+			std::list<geometry_type>::iterator it1 = input_container.begin();
+			std::list<SpatialCoords>::iterator it2 = displacement_container.begin();
 
 			while (it1 != input_container.end() && it2 != displacement_container.end()) {
 				bg::strategy::transform::translate_transformer<double, 2, 2> trans_obj(it2->xc, it2->yc);
 				out_container.push_back(translate_geometry(*it1, trans_obj));
 				it1++;
-				it2++;
+				if (displacement_container.size() != 1) it2++;
+			}
+			return out_container;
+		}
+
+		template <typename geometry_type>
+		geometry_type affine_transform_geometry(geometry_type& in_geometry, bg::strategy::transform::matrix_transformer<double, 2, 2>& trans_obj) {
+			geometry_type out_geom;
+			boost::geometry::transform(in_geometry, out_geom, trans_obj);
+			return out_geom;
+		}
+
+		template<typename geometry_type>
+		std::vector<geometry_type> affine_transform_geometries(std::vector<geometry_type>& input_container,
+			std::vector<bg::strategy::transform::matrix_transformer<double, 2, 2>>& transformation_container) {
+
+			assert(input_container.size() == transformation_container.size() || transformation_container.size() == 1, "Size mismatch of input containers!");
+			std::vector<geometry_type> out_container; out_container.reserve(input_container.size());
+
+			auto it1 = input_container.begin();
+			auto it2 = transformation_container.begin();
+
+			while (it1 != input_container.end() && it2 != transformation_container.end()) {
+				bg::strategy::transform::matrix_transformer<double, 2, 2>& trans_obj = *it1;
+				geometry_type transformed_geom = translate_geometry(*it1, trans_obj);
+				out_container.push_back(transformed_geom);
+				it1++;
+				if (displacement_container.size() != 1) it2++;
 			}
 			return out_container;
 
 		}
+		
 
 	}
 }
