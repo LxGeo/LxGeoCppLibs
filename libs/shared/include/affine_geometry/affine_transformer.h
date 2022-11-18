e1134dcb005c80681995d99277d9993296723ece
sherif-med <cherif.abedrazek@gmail.com>
2022-11-18 15:41:05 +0100

    remove api export from header file

--- libs/shared/include/affine_geometry/affine_transformer.h
+++ libs/shared/include/affine_geometry/affine_transformer.h
@@ -9,7 +9,7 @@ namespace LxGeo
 	{
 
 		template <typename geometry_type>
-		LX_GEO_FACTORY_SHARED_API geometry_type translate_geometry(geometry_type& in_geometry, bg::strategy::transform::translate_transformer<double, 2, 2>& trans_obj) {
+		geometry_type translate_geometry(geometry_type& in_geometry, bg::strategy::transform::translate_transformer<double, 2, 2>& trans_obj) {
 			geometry_type out_geom;
 			boost::geometry::transform(in_geometry, out_geom, trans_obj);
 			return out_geom;
@@ -17,7 +17,7 @@ namespace LxGeo
 
 		//template<template <class> class container_type, class geometry_type>
 		template<typename geometry_type>
-		LX_GEO_FACTORY_SHARED_API std::vector<geometry_type> translate_geometries(std::vector<geometry_type>& input_container, std::vector<SpatialCoords>& displacement_container) {
+		std::vector<geometry_type> translate_geometries(std::vector<geometry_type>& input_container, std::vector<SpatialCoords>& displacement_container) {
 
 			assert(input_container.size() == displacement_container.size() || displacement_container.size()==1, "Size mismatch of input containers!");
 			std::vector<geometry_type> out_container; out_container.reserve(input_container.size());
@@ -38,7 +38,7 @@ namespace LxGeo
 
 
 		template<typename geometry_type>
-		LX_GEO_FACTORY_SHARED_API std::list<geometry_type> translate_geometries(std::list<geometry_type>& input_container, std::list<SpatialCoords>& displacement_container) {
+		std::list<geometry_type> translate_geometries(std::list<geometry_type>& input_container, std::list<SpatialCoords>& displacement_container) {
 
 			assert(input_container.size() == displacement_container.size() || displacement_container.size() == 1, "Size mismatch of input containers!");
 			std::list<geometry_type> out_container;
@@ -63,7 +63,7 @@ namespace LxGeo
 		}
 		*/
 		template<typename geometry_type>
-		LX_GEO_FACTORY_SHARED_API std::vector<geometry_type> affine_transform_geometries(std::vector<geometry_type>& input_container,
+		std::vector<geometry_type> affine_transform_geometries(std::vector<geometry_type>& input_container,
 			std::vector<bg::strategy::transform::matrix_transformer<double, 2, 2>>& transformation_container) {
 
 			assert(input_container.size() == transformation_container.size() || transformation_container.size() == 1, "Size mismatch of input containers!");
@@ -85,7 +85,7 @@ namespace LxGeo
 		
 
 		template <typename in_geometry_type, typename out_geometry_type>
-		LX_GEO_FACTORY_SHARED_API out_geometry_type affine_transform_geometry(in_geometry_type& in_geometry, bg::strategy::transform::matrix_transformer<double, 2, 2>& trans_obj) {
+		out_geometry_type affine_transform_geometry(in_geometry_type& in_geometry, bg::strategy::transform::matrix_transformer<double, 2, 2>& trans_obj) {
 			out_geometry_type out_geom;
 			boost::geometry::transform(in_geometry, out_geom, trans_obj);
 			return out_geom;
