f73fd3a29176400831a8686700afa4ba489761fc
unknown <macherif@luxcarta.com>
2023-03-07 11:09:35 +0100

    fixing _calc_ pixel & spatial coords && adding to_file && from_file methods && adding rotate geoimage inplace (keep spatial position only rotate pixel array)

--- libs/IO_DATA/include/lightweight/geoimage.h
+++ libs/IO_DATA/include/lightweight/geoimage.h
@@ -24,7 +24,7 @@ namespace LxGeo
 				set_geotransform(_geotransform);
 			}
 
-			void set_geotransform(double _geotransform[6]) {
+			void set_geotransform(const double _geotransform[6]) {
 				for (size_t g_idx = 0; g_idx < 6; g_idx++)
 					geotransform[g_idx] = _geotransform[g_idx];
 			}
@@ -52,22 +52,29 @@ namespace LxGeo
 			//}
 
 			template <typename coord_type>
-			void _calc_pixel_coords(const double& sc_x, const double& sc_y, coord_type& px_col, coord_type& px_row) {
-				double& px = geotransform[0];
-				double& py = geotransform[3];
-				double& rx = geotransform[1];
-				double& ry = geotransform[5];
-				px_col = static_cast<coord_type>((sc_x - px) / rx);
-				px_row = static_cast<coord_type>((sc_y - py) / ry);
+			void _calc_pixel_coords(const double& sc_x, const double& sc_y, coord_type& px_col, coord_type& px_row) const  {
+				const double& px = geotransform[0];
+				const double& py = geotransform[3];
+				const double& rx = geotransform[1];
+				const double& ry = geotransform[5];
+				const double& nx = geotransform[2];
+				const double& ny = geotransform[4];
+				//px_col = static_cast<coord_type>((sc_x - px) / rx);
+				//px_row = static_cast<coord_type>((sc_y - py) / ry);
+
+				px_col = (ny) ? static_cast<coord_type>((sc_x - px) / rx + (sc_y - py) / ny) : static_cast<coord_type>((sc_x - px) / rx);
+				px_row = (nx) ? static_cast<coord_type>((sc_y - py) / ry + (sc_x - px) / nx) : static_cast<coord_type>((sc_y - py) / ry);
 			}
 
-			void _calc_spatial_coords(const int& px_col, const int& px_row, double& sc_x, double& sc_y) {
-				double& px = geotransform[0];
-				double& py = geotransform[3];
-				double& rx = geotransform[1];
-				double& ry = geotransform[5];
-				sc_x = px_col * rx + px;
-				sc_y = px_row * ry + py;
+			void _calc_spatial_coords(const int& px_col, const int& px_row, double& sc_x, double& sc_y) const {
+				const double& px = geotransform[0];
+				const double& py = geotransform[3];
+				const double& rx = geotransform[1];
+				const double& ry = geotransform[5];
+				const double& nx = geotransform[2];
+				const double& ny = geotransform[4];
+				sc_x = px_col * rx + px + px_row*nx;
+				sc_y = px_row * ry + py + px_col*ny;
 			}
 
 			template <typename cv_mat_type>
@@ -107,6 +114,59 @@ namespace LxGeo
 				const int row_start = (int)std::round(row_start_subpixel), row_end = (int)std::round(row_end_subpixel);
 				return get_view_pixel<cv_mat_type>(col_start, row_start, col_end - col_start, row_end - row_start);
 			}
+
+			void to_file(const std::string& out_file, OGRSpatialReference* spatial_refrence = nullptr, GDALDataType* ptr_out_data_type = nullptr) {
+				KGDAL2CV kgdal2cv;
+
+				// create gdal dataset
+				GDALDriver* tiff_driver = GetGDALDriverManager()->GetDriverByName("GTiff");
+				GDALDataType out_data_type;
+				if (ptr_out_data_type) out_data_type = *ptr_out_data_type;
+				else out_data_type = kgdal2cv.opencv2gdal(image.type());
+				
+				GDALDataset* new_dataset = tiff_driver->Create(out_file.c_str(), image.cols, image.rows, image.channels(), out_data_type, NULL);
+				if (new_dataset == NULL) { throw std::exception(fmt::format("Cannot create dataset at {}", out_file).c_str()); }
+				if (spatial_refrence) new_dataset->SetSpatialRef(spatial_refrence);
+				
+				new_dataset->SetGeoTransform(geotransform);
+				cv::Mat to_save_image;
+				if constexpr (std::is_same_v<cv_mat_type, cv::Mat>)
+					to_save_image = image;
+				else
+					image.download(to_save_image);
+
+				to_save_image.convertTo(to_save_image, kgdal2cv.gdal2opencv(out_data_type, image.channels()));
+				kgdal2cv.ImgWriteByGDAL(new_dataset, to_save_image, 0, 0);
+				GDALClose(new_dataset);
+			}
+
+			static GeoImage<cv_mat_type> from_file(const std::string& in_file, const OGREnvelope& spatial_envelope) {
+				GeoImage<cv_mat_type> loaded_gimg;
+				GDALDataset* raster_dataset = (GDALDataset*)GDALOpen(in_file.c_str(), GA_ReadOnly);
+				raster_dataset->GetGeoTransform(loaded_gimg.geotransform);
+				GDALClose((GDALDatasetH)raster_dataset);
+				int col_start_pixel, col_end_pixel, row_start_pixel, row_end_pixel;
+				loaded_gimg._calc_pixel_coords(spatial_envelope.MinX, spatial_envelope.MaxY, col_start_pixel, row_start_pixel);
+				loaded_gimg._calc_pixel_coords(spatial_envelope.MaxX, spatial_envelope.MinY, col_end_pixel, row_end_pixel);
+				KGDAL2CV kgdal2cv;
+				cv::Mat loaded_image = kgdal2cv.PaddedImgReadByGDAL(in_file, col_start_pixel, row_start_pixel, col_end_pixel-col_start_pixel, row_end_pixel-row_start_pixel);
+				loaded_gimg.set_image(loaded_image);
+				// fix geotransform because of padding
+				loaded_gimg.geotransform[0] = spatial_envelope.MinX; loaded_gimg.geotransform[3] = spatial_envelope.MaxY;
+				return loaded_gimg;
+			}
+
+			static GeoImage<cv_mat_type> from_file(const std::string& in_file) {
+				GeoImage<cv_mat_type> loaded_gimg;
+				GDALDataset* raster_dataset = (GDALDataset*)GDALOpen(in_file.c_str(), GA_ReadOnly);
+				raster_dataset->GetGeoTransform(loaded_gimg.geotransform);
+				GDALClose((GDALDatasetH)raster_dataset);
+				KGDAL2CV kgdal2cv;
+				cv::Mat loaded_image = kgdal2cv.ImgReadByGDAL(in_file);
+				loaded_gimg.set_image(loaded_image);
+				return loaded_gimg;
+			}
+
 		};
 
 		template <typename cv_mat_type>
@@ -120,6 +180,37 @@ namespace LxGeo
 			bounds.MinX = std::min<double>(xs, xe); bounds.MaxX = std::max<double>(xs, xe);
 			bounds.MinY = std::min<double>(ys, ye); bounds.MaxY = std::max<double>(ys, ye);
 			return bounds;
+		}
+
+
+		template <typename cv_mat_type>
+		GeoImage<cv_mat_type> rotate(const GeoImage<cv_mat_type>& gimg, const double& rotation_angle) {
+			// get rotation matrix for rotating the image around its center
+			cv::Point2f rotation_center(gimg.image.cols / 2.0, gimg.image.rows / 2.0);
+			cv::Mat rot = cv::getRotationMatrix2D(rotation_center, rotation_angle, 1.0);
+			// determine bounding rectangle
+			cv::Rect bbox = cv::RotatedRect(rotation_center, gimg.image.size(), rotation_angle).boundingRect();
+			// adjust transformation matrix
+			rot.at<double>(0, 2) += bbox.width / 2.0 - rotation_center.x;
+			rot.at<double>(1, 2) += bbox.height / 2.0 - rotation_center.y;
+
+			GeoImage<cv_mat_type> rotated_gimg;
+			if constexpr (std::is_same_v<cv_mat_type, cv::Mat>) {
+				// Behavior for cpu_matrix type
+				cv::warpAffine(gimg.image, rotated_gimg.image, rot, bbox.size(), cv::INTER_CUBIC);
+			}
+			else if constexpr (std::is_same_v<cv_mat_type, cv::cuda::GpuMat>) {
+				// Behavior for gpu_matrix type
+				cv::cuda::warpAffine(gimg.image, rotated_gimg.image, rot, bbox.size(), cv::INTER_CUBIC);
+			}
+
+			// assign updated geotransform
+			cv::Mat rot_inv; cv::invertAffineTransform(rot, rot_inv);
+			cv::Mat input_geotransform_as_matrix; transform_G2CV_affine(gimg.geotransform, input_geotransform_as_matrix);
+			cv::Mat product_mat = multiply_affine_matrices(input_geotransform_as_matrix, rot_inv);
+			transform_CV2G_affine(product_mat, rotated_gimg.geotransform);
+
+			return rotated_gimg;
 		}
 
 
