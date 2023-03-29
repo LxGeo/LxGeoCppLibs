db1689fb4488dcabdacc74c35052995f8a4aa0cb
unknown <macherif@luxcarta.com>
2023-03-29 11:56:17 +0200

    changing rasterize_individual_masks return type to a tuple of geoimage and a vector of geometries labels && using RProfile instead of RasterIO as reference raster info && using general spatial weights && removing all touched at rasterization && commenting geometries masks retriveal using bitwise operation

--- libs/shared/include/geometry_rasterizer/individual_masks.h
+++ libs/shared/include/geometry_rasterizer/individual_masks.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "lightweight/geoimage.h"
+#include "lightweight/raster_profile.h"
 #include "io_raster.h"
 #include "defs_opencv.h"
-#include "graph_weights/polygons_spatial_weights.h"
+#include "graph_weights/spatial_weights.h"
 #include "gdal_algs_wrap/gdal_rasterize.h"
 #include "export_shared.h"
 
@@ -12,9 +13,9 @@ namespace LxGeo
 	namespace GeometryFactoryShared
 	{
 
-		// parent dataset is used to get SRS & geotransform for correct overlay
+		// Returns a pair of geoimage of rasterized geometries and a vector of respective labels
 		template <typename geometry_type, typename cv_mat_type>
-		std::vector<IO_DATA::GeoImage<cv_mat_type>> rasterize_geometries_masks(std::vector<geometry_type>& geometry_container, IO_DATA::RasterIO& reference_raster) {
+		std::pair<IO_DATA::GeoImage<cv_mat_type>, std::vector<size_t>> rasterize_geometries_masks(std::vector<geometry_type>& geometry_container, const IO_DATA::RProfile& reference_raster_profile) {
 
 			std::vector<GeoImage<cv_mat_type>> output_masks; output_masks.reserve(geometry_container.size());
 
@@ -23,9 +24,8 @@ namespace LxGeo
 
 			// since labels count should not be high (limited by the value type) \\ for example; for 64 geometries we will need 2**64 labels
 			// we reset labels for every connected component
-			std::unordered_map<size_t, size_t> in_component_labels;
-			// TODO: implement all geometries spatial weights
-			PolygonSpatialWeights PSW = PolygonSpatialWeights(geometry_container);
+			std::unordered_map<size_t, size_t> in_component_labels;			
+			SpatialWeights<geometry_type> PSW = SpatialWeights<geometry_type>(geometry_container);
 			WeightsDistanceBandParams wdbp = { 0, true, -1, [](double x)->double { return x; } };
 			PSW.fill_distance_band_graph(wdbp);
 			PSW.run_labeling();
@@ -50,6 +50,12 @@ namespace LxGeo
 			{
 				printf("Creation of output file failed.\n");
 				exit(1);
+			}
+
+			if (!reference_raster_profile.s_crs_wkt.empty()) {
+				OGRSpatialReference srs;
+				srs.importFromWkt(reference_raster_profile.s_crs_wkt.c_str());
+				datasource->SetSpatialRef(&srs);
 			}
 
 			OGRwkbGeometryType wkb_type;
@@ -93,10 +99,11 @@ namespace LxGeo
 			
 
 			//**************** Create meomory output raster **************//
-			std::string raster_output_path = random_string(5, "/vsimem/", ".tif");
-			OGREnvelope output_envelope = transform_B2OGR_Envelope(reference_raster.bounding_box);
+			std::string raster_output_path = random_string(5, "/vsimem/", ".tif");			
+			
+			OGREnvelope output_envelope = transform_B2OGR_Envelope(reference_raster_profile.to_box_extents());
 			const std::list<std::string> extra_options = {
-				"-at", //all touched
+				//"-at", //all touched
 				"-add", //necessary to differentiate between rasterized polygons
 				"-a", "label"
 			};
@@ -105,39 +112,32 @@ namespace LxGeo
 			
 			rasterize_shapefile(
 				raster_output_path, temp_shapefile_path, &output_envelope,
-				reference_raster.get_pixel_width(), reference_raster.get_pixel_height(),
+				std::abs(reference_raster_profile.geotransform[1]), std::abs(reference_raster_profile.geotransform[5]),
 				extra_options, &no_data_value, output_datatype
 			);
 
-			/*
-			GDALDataset* pSrcDataset = static_cast<GDALDataset*>(GDALOpen(raster_output_path.c_str(), GA_ReadOnly));
-			GDALDriver* ESRIDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
-			GDALDataset* jpeg_ds = ESRIDriver->CreateCopy("C:/DATA_SANDBOX/maskedMatching/LA/sample1/matching_output/result.tif", pSrcDataset, false, NULL, GDALDummyProgress, NULL);
-			GDALClose(jpeg_ds);
-			*/
+			GeoImage<cv_mat_type> masks_geoimage = GeoImage<cv_mat_type>::from_file(raster_output_path);
+				/*for (size_t geometry_idx = 0; geometry_idx < geometry_container.size(); geometry_idx++) {
+					Boost_Box_2 c_geometry_bounds;
+					boost::geometry::envelope(geometry_container[geometry_idx], c_geometry_bounds);
+					GeoImage<cv_mat_type> c_geom_mask = masks_geoimage.get_view_spatial<cv_mat_type>(
+						c_geometry_bounds.min_corner().get<0>(), c_geometry_bounds.min_corner().get<1>(),
+						c_geometry_bounds.max_corner().get<0>(), c_geometry_bounds.max_corner().get<1>()
+					);
 
-			IO_DATA::RasterIO masks_raster(raster_output_path, GA_ReadOnly, false);
-			GeoImage<cv::cuda::GpuMat> masks_geoimage(masks_raster.raster_data, masks_raster.geotransform);
-			for (size_t geometry_idx = 0; geometry_idx < geometry_container.size(); geometry_idx++) {				
-				Boost_Box_2 c_geometry_bounds;
-				boost::geometry::envelope(geometry_container[geometry_idx], c_geometry_bounds);
-				GeoImage<cv::cuda::GpuMat> c_geom_mask = masks_geoimage.get_view_spatial<cv::cuda::GpuMat>(
-					c_geometry_bounds.min_corner().get<0>(), c_geometry_bounds.min_corner().get<1>(),
-					c_geometry_bounds.max_corner().get<0>(), c_geometry_bounds.max_corner().get<1>()
-				);
 
-				//cv::cuda::GpuMat image1(81, 38, c_geom_mask.image.type(), 64), temp_mat1(81, 38, c_geom_mask.image.type(), 2621), out_mat1(81, 38, c_geom_mask.image.type(),17);
-				//image1.setTo(2); temp_mat1.setTo(6); out_mat1.setTo(0);
-				//cv::cuda::bitwise_and(image1, temp_mat1, out_mat1);
+					cv_mat_type temp_mat = cv_mat_type(c_geom_mask.image.size(), c_geom_mask.image.type()); temp_mat.setTo(int(geometries_labels_vector[geometry_idx]));
+					cv_mat_type out_mat(c_geom_mask.image.size(), c_geom_mask.image.type());
+					if constexpr (std::is_same_v<cv_mat_type, cv::cuda::GpuMat>)
+						cv::cuda::bitwise_and(c_geom_mask.image.clone(), temp_mat, out_mat);
+					else
+						cv::bitwise_and(c_geom_mask.image, temp_mat, out_mat);
 
-				cv_mat_type temp_mat = cv_mat_type(c_geom_mask.image.size(), c_geom_mask.image.type()); temp_mat.setTo(int(geometries_labels_vector[geometry_idx]));
-				cv_mat_type out_mat(c_geom_mask.image.size(), c_geom_mask.image.type());
-				cv::cuda::bitwise_and(c_geom_mask.image.clone(), temp_mat, out_mat);
-				c_geom_mask.set_image( out_mat );
-				GeoImage<cv_mat_type> to_push_geoimage(out_mat, c_geom_mask.geotransform);
-				output_masks.push_back(to_push_geoimage);
-			}
-			return output_masks;
+					c_geom_mask.set_image( out_mat );
+					output_masks.push_back(c_geom_mask);
+				}*/
+			auto output_pair = std::make_pair(masks_geoimage, geometries_labels_vector);
+			return output_pair;
 		}
 	}
 }
