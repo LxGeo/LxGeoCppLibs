7bf8aadc5529a5ee29dfdaa8cd825e12a67cdc32
sherif-med <cherif.abedrazek@gmail.com>
2023-04-02 18:19:54 +0200

    fixing default output type typo

--- libs/shared/include/gdal_algs_wrap/gdal_proximity.h
+++ libs/shared/include/gdal_algs_wrap/gdal_proximity.h
@@ -14,7 +14,7 @@ namespace LxGeo
 		* Gdal proximity runs a gdal proximity on a geoImage and returns a geoimage
 		*/
 		LX_GEO_FACTORY_SHARED_API GeoImage<cv::Mat> proximity_raster(const GeoImage<cv::Mat>& input_image, size_t source_band_idx = 1, const std::list<double>& target_pixels = {},
-			GDALDataType output_type = GDT_CFloat32, std::string distunits = "GEO", std::optional<double>maxdist = std::optional<double>(),
+			GDALDataType output_type = GDT_Float32, std::string distunits = "GEO", std::optional<double>maxdist = std::optional<double>(),
 			std::optional<double> nodata = std::optional<double>(), std::optional<double> fixed_buf_val = std::optional<double>(), const std::list<std::string>& extra_options = {});
 
 
