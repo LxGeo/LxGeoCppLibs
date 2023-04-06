b269a49a0c4743f45419da6f8cc315b03a4e512d
sherif-med <cherif.abedrazek@gmail.com>
2023-04-06 12:37:25 +0200

    fixing coords aggergation function typos && stats calc typos && (thinking to remove this class)

--- libs/shared/include/coords.h
+++ libs/shared/include/coords.h
@@ -18,16 +18,16 @@ struct PixelCoords {
 };
 
 
-auto spatial_coords_mean_aggregator = [](std::list<SpatialCoords>& pts_coords) {
+inline std::function<SpatialCoords(std::list<SpatialCoords>&)> spatial_coords_mean_aggregator = [](std::list<SpatialCoords>& pts_coords)->SpatialCoords {
 	SpatialCoords mean_coord = { 0.0,0.0 };
 	for (auto& c_coord : pts_coords) mean_coord += (c_coord / pts_coords.size());
 	return mean_coord;
 };
 
-auto spatial_coords_weighted_mean_aggregator = [](std::list<SpatialCoords>& pts_coords, std::list<double>& pts_weights) {
+inline std::function<SpatialCoords(std::list<SpatialCoords>&, std::list<double>&)> spatial_coords_weighted_mean_aggregator = [](std::list<SpatialCoords>& pts_coords, std::list<double>& pts_weights) {
 	SpatialCoords mean_coord = { 0.0,0.0 };
-	auto& coords_iter = pts_coords.begin();
-	auto& weight_iter = pts_weights.begin();
+	auto coords_iter = pts_coords.begin();
+	auto weight_iter = pts_weights.begin();
 	double sum_weight = 0;
 	for (; coords_iter != pts_coords.end() && weight_iter != pts_weights.end(); ++coords_iter, ++weight_iter) {
 		mean_coord += (*coords_iter) * (*weight_iter);
@@ -36,19 +36,19 @@ auto spatial_coords_weighted_mean_aggregator = [](std::list<SpatialCoords>& pts_
 	return mean_coord/ sum_weight;
 };
 
-auto median = [](std::vector<double>& values)->double {
+inline auto median = [](std::vector<double>& values)->double {
 	if (values.empty())
 		return 0.0;
 
 	if (values.size() % 2 == 0) {
-		const auto median_it1 = values.begin() + values.size() / 2 - 1;
-		const auto median_it2 = values.begin() + values.size() / 2;
+		auto median_it1 = values.begin() + values.size() / 2 - 1;
+		auto median_it2 = values.begin() + values.size() / 2;
 
 		std::nth_element(values.begin(), median_it1, values.end());
-		const auto e1 = *median_it1;
+		auto e1 = *median_it1;
 
 		std::nth_element(values.begin(), median_it2, values.end());
-		const auto e2 = *median_it2;
+		auto e2 = *median_it2;
 
 		return (e1 + e2) / 2;
 
@@ -60,11 +60,11 @@ auto median = [](std::vector<double>& values)->double {
 	}
 };
 
-auto spatial_coords_median_aggregator = [](std::list<SpatialCoords>& pts_coords) {
+inline std::function<SpatialCoords(const std::list<SpatialCoords>&)> spatial_coords_median_aggregator = [](const std::list<SpatialCoords>& pts_coords) {
 
 	std::vector<double> xcs, ycs; xcs.reserve(pts_coords.size()); ycs.reserve(pts_coords.size());
 
-	for(auto& sc: pts_coords) {
+	for(const auto& sc: pts_coords) {
 		if (sc.xc!=0) xcs.push_back(sc.xc);
 		if (sc.yc != 0) ycs.push_back(sc.yc);
 	}
