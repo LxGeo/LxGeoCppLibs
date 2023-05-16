26f684c90444d90700d655db10f94ee7ce82c96e
sherif-med <cherif.abedrazek@gmail.com>
2023-05-16 17:32:02 +0200

    adding polygon simplification algorithm using polygon traits and new circular iterator

--- libs/shared/include/geometry_lab.h
+++ libs/shared/include/geometry_lab.h
@@ -4,6 +4,7 @@
 #include "defs_cgal.h"
 #include "defs_common.h"
 #include "export_shared.h"
+#include "design_pattern/circular_range_wrapper.h"
 
 namespace LxGeo
 {
@@ -34,7 +35,7 @@ namespace LxGeo
 			*  @param polygon_ring: a vector representing closed polygon ring points.
 			*  @return a vector of simplified polygon 'closed'
 			*/
-		LX_GEO_FACTORY_SHARED_API std::vector<Inexact_Point_2> simplify_aberrant_polygon(const std::vector<Inexact_Point_2>& polygon_ring);
+		/*LX_GEO_FACTORY_SHARED_API std::vector<Inexact_Point_2> simplify_aberrant_polygon(const std::vector<Inexact_Point_2>& polygon_ring);*/
 
 		/**
 			*  Function to turn single part self-intersecting polygon into a list of non sel-intersecting polygons.
@@ -46,6 +47,61 @@ namespace LxGeo
 		LX_GEO_FACTORY_SHARED_API std::list<Inexact_Polygon_2> explose_self_intersecting_polygon(const Inexact_Polygon_2& polygon_ring);
 
 		
+		template <typename Iterator, template <typename...> class Container = std::vector>
+		Container<typename std::iterator_traits<Iterator>::value_type> simplify_aberrant_ring(Iterator begin, Iterator end) {
+
+			using point_type = typename std::iterator_traits<Iterator>::value_type;
+			Container<point_type> simplified_R; //simplified_R.reserve(in_ring.size());
+
+			auto circ_iter = CopyCircularWrapper<std::list<point_type>>(begin, std::prev(end));
+									
+			auto c_view = circ_iter.getRangeIterators(circ_iter.begin(), 1, 1);
+
+			auto turn_m2 = c_view[0];
+			auto turn_m1 = c_view[1];
+			auto turn_m0 = c_view[2];
+
+			while (simplified_R.size() < circ_iter.size()) {
+				if (!pts_collinear_2(*turn_m0, *turn_m1, *turn_m2)) {
+					simplified_R.push_back(*turn_m1);
+					turn_m2++;
+					turn_m1++;
+					turn_m0++;
+				}
+				else {
+					turn_m1 = circ_iter.erase(turn_m1);
+					turn_m1--;
+					if (!simplified_R.empty()) simplified_R.pop_back();
+					if (circ_iter.size() == 0)
+						continue;
+					turn_m0 = turn_m1; turn_m0++;
+					turn_m2 = turn_m1; turn_m2--;
+				}
+			}
+			if (simplified_R.size() > 0) simplified_R.push_back(simplified_R.front());
+			return simplified_R;
+			
+			
+		}
+
+		template <typename polygonType>
+		polygonType simplify_aberrant_polygon(const polygonType& polygon) {
+
+			using PointType = typename PolygonTrait<polygonType>::PointType;
+
+			std::vector<PointType> exterior_ring = PolygonTrait<polygonType>::getOuterRing(polygon);
+			std::vector<PointType> simplified_exterior_ring = simplify_aberrant_ring(exterior_ring.begin(), exterior_ring.end());
+
+			std::vector<std::vector<PointType>> simplified_interior_rings;
+			for (size_t c_interior_idx = 0; c_interior_idx < PolygonTrait<polygonType>::interiorCount(polygon); c_interior_idx++) {
+				std::vector<PointType> c_interior_ring = PolygonTrait<polygonType>::getInnerRing(polygon, c_interior_idx);
+				simplified_interior_rings.push_back(simplify_aberrant_ring(c_interior_ring.begin(), c_interior_ring.end()));
+			}
+
+			polygonType simplified_polygon = PolygonTrait<polygonType>::create(simplified_exterior_ring, simplified_interior_rings);
+
+			return simplified_polygon;
+		}
 
 		LX_GEO_FACTORY_SHARED_API double max_distance_between_lines(const Inexact_Point_2& p1,
 			const Inexact_Point_2& p2,
